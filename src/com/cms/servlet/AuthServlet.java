d6cf736899c1ce6f0870fe19b9b55aa7c3f3d1fc
BhushithaHashan <bhushithahashan99@gmail.com>
2025-06-17 17:18:26 +0530

    authserlvet modified

--- src/com/cms/servlet/AuthServlet.java
+++ src/com/cms/servlet/AuthServlet.java
@@ -1,10 +1,9 @@
 package com.cms.servlet;
 
-
-import java.io.*;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.io.*;
 import java.sql.*;
 
 @WebServlet("/login")
@@ -13,28 +12,20 @@ public class AuthServlet extends HttpServlet {
         String u = req.getParameter("username");
         String p = req.getParameter("password");
 
-        try (Connection con = DBUtil.getConnection()) {
+        try(Connection con = DBUtil.getConnection(getServletContext())) {
             PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE username=? AND password=?");
-            ps.setString(1, u);
-            ps.setString(2, p);
+            ps.setString(1,u);
+            ps.setString(2,p);
             ResultSet rs = ps.executeQuery();
 
-            if (rs.next()) {
+            if(rs.next()){
                 HttpSession session = req.getSession();
                 session.setAttribute("userId", rs.getInt("id"));
                 session.setAttribute("role", rs.getString("role"));
 
-                if ("admin".equals(rs.getString("role"))) {
-                    resp.sendRedirect("admin_home.jsp");
-                } else {
-                    resp.sendRedirect("employee_home.jsp");
-                }
-            } else {
-                resp.sendRedirect("error.jsp");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            resp.sendRedirect("error.jsp");
-        }
+                if("admin".equals(rs.getString("role"))) resp.sendRedirect("admin_home.jsp");
+                else resp.sendRedirect("employee_home.jsp");
+            } else resp.sendRedirect("error.jsp");
+        } catch(Exception e){ e.printStackTrace(); resp.sendRedirect("error.jsp"); }
     }
 }
