9d48c317532048558e316ea78e7a73de92a75766
BhushithaHashan <bhushithahashan99@gmail.com>
2025-06-17 17:31:23 +0530

    complaintserlvet modified

--- src/com/cms/servlet/ComplaintServlet.java
+++ src/com/cms/servlet/ComplaintServlet.java
@@ -1,9 +1,9 @@
 package com.cms.servlet;
 
-import java.io.*;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.io.*;
 import java.sql.*;
 
 @WebServlet("/complaint")
@@ -13,16 +13,16 @@ public class ComplaintServlet extends HttpServlet {
         HttpSession session = req.getSession();
         int userId = (int) session.getAttribute("userId");
 
-        try (Connection con = DBUtil.getConnection()) {
-            if ("add".equals(action)) {
+        try(Connection con = DBUtil.getConnection(getServletContext())) {
+            if("add".equals(action)){
                 PreparedStatement ps = con.prepareStatement("INSERT INTO complaints(user_id,title,description) VALUES(?,?,?)");
-                ps.setInt(1, userId);
-                ps.setString(2, req.getParameter("title"));
-                ps.setString(3, req.getParameter("description"));
+                ps.setInt(1,userId);
+                ps.setString(2,req.getParameter("title"));
+                ps.setString(3,req.getParameter("description"));
                 ps.executeUpdate();
                 resp.sendRedirect("employee_home.jsp");
 
-            } else if ("update".equals(action)) {
+            } else if("update".equals(action)){
                 PreparedStatement ps = con.prepareStatement("UPDATE complaints SET status=?, remarks=? WHERE id=?");
                 ps.setString(1, req.getParameter("status"));
                 ps.setString(2, req.getParameter("remarks"));
@@ -30,20 +30,13 @@ public class ComplaintServlet extends HttpServlet {
                 ps.executeUpdate();
                 resp.sendRedirect("admin_home.jsp");
 
-            } else if ("delete".equals(action)) {
+            } else if("delete".equals(action)){
                 PreparedStatement ps = con.prepareStatement("DELETE FROM complaints WHERE id=?");
-                ps.setInt(1, Integer.parseInt(req.getParameter("id")));
+                ps.setInt(1,Integer.parseInt(req.getParameter("id")));
                 ps.executeUpdate();
-
-                if ("admin".equals(session.getAttribute("role"))) {
-                    resp.sendRedirect("admin_home.jsp");
-                } else {
-                    resp.sendRedirect("employee_home.jsp");
-                }
+                if("admin".equals(session.getAttribute("role"))) resp.sendRedirect("admin_home.jsp");
+                else resp.sendRedirect("employee_home.jsp");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            resp.sendRedirect("error.jsp");
-        }
+        } catch(Exception e){ e.printStackTrace(); resp.sendRedirect("error.jsp"); }
     }
 }
